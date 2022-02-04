5535f221c34026ae8af2997860fa0aba863a04e5
chetan joshi <chtnjsh7@gmail.comgit config--listgit config --global user.email chtnjsh7@gmail.com>
2022-02-04 11:01:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function showdiv(){
+    document.getElementById("interval-1").style.visibility= "visible";
+}
+setTimeout((showdiv),2000);;
+
+
+function elipsShow(){
+    document.getElementById("interval-2").style.visibility= "visible";
+}
+setTimeout((elipsShow),2500);;
+ const Botclick = () =>{
+    function showBox(){
+        document.getElementById("interval-9").style.visibility= "visible";
+     }
+     setTimeout((showBox),3000);;
+   
+   
+   function ShowFaq(){
+       document.getElementById("interval-3").style.visibility= "visible";
+   }
+   setTimeout((ShowFaq),4000);;
+   function ShowFb(){
+       document.getElementById("interval-4").style.visibility= "visible";
+   }
+   setTimeout((ShowFb),5000);;
+   function ShowQuery(){
+       document.getElementById("interval-5").style.visibility= "visible";
+   }
+   setTimeout((ShowQuery),6000);;
+   function ShowQuery1(){
+       document.getElementById("interval-6").style.visibility= "visible";
+   }
+   setTimeout((ShowQuery1),7000);;
+   function ShowQuery2(){
+       document.getElementById("interval-7").style.visibility= "visible";
+   }
+   setTimeout((ShowQuery2),8000);;
+   function ShowQuery3(){
+       document.getElementById("interval-8").style.visibility= "visible";
+   }
+   setTimeout((ShowQuery3),9000);;
+   
+
+ }
+ 
